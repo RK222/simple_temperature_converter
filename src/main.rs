384448408d322a6e00f5d76039d6522a0235302b
RK222 <56446817+RK222@users.noreply.github.com>
2020-10-11 00:27:48 -0400

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@ use std::io;
 use regex::Regex;
 use std::process::exit;
 
+// conversion factors, see convert function
 const CTF: f64 = 9.0 / 5.0;
 const FTC: f64 = 5.0 / 9.0;
 
 fn main() {
-
     println!("Temperature Converter (C/F)");
     println!("Please enter a temperature to convert:");
 
+    let re = Regex::new(r"\d+(\.\d+)??[cCfF]").unwrap(); // ex. 212F, 0.00C, 123456.789f
+
     loop {
+        // collect user input
         let mut input = String::new();
-        let re = Regex::new(r"\d+(\.\d+)??[cCfF]").unwrap();
 
         loop {
 		    io::stdin()
@@ -29,19 +31,15 @@ fn main() {
         }
     
         // clean input, extract information
-
         let mut clean_input = input.trim().to_lowercase();
     
         let units = clean_input.pop().unwrap();
         let val = clean_input.parse::<f64>().unwrap();
 
-
-        // run convert function
-
+        // convert temperature
         let result = convert(val, units);
 
         // print result
-
         println!("{:.2}{}\n", result.0, result.1)
     }
 }
